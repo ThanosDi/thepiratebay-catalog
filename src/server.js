e86a723a6c812ff048c6cc7f9e4153d11165c6d9
ThanosDi <tdiamantidis@vivantehealth.com>
2020-12-28 16:51:40 +0200

    Add process.env.PORT

--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ const {serveHTTP} = require('stremio-addon-sdk');
 require('events').EventEmitter.defaultMaxListeners = 50;
 const ONE_DAY = 86400;
 const addonInterface = require('./addon');
-serveHTTP(addonInterface, {port: 7000, cacheMaxAge: 1}); // cacheMaxAge: 10000
+serveHTTP(addonInterface, {port: process.env.PORT | 7000, cacheMaxAge: 1}); // cacheMaxAge: 10000
