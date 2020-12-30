fb57d0cfecd765e003078767ca9de273d130a759
ThanosDi <tdiamantidis@vivantehealth.com>
2020-12-30 12:36:11 +0200

    Add dockerfile

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ const addonInterface = require('./addon');
 
 require('events').EventEmitter.defaultMaxListeners = 50;
 const ONE_DAY = 86400;
-
-serveHTTP(addonInterface, {
-    port: process.env.PORT || 7000,
-    cacheMaxAge: 1
-}); // cacheMaxAge: 10000
+try {
+    serveHTTP(addonInterface, {
+        port: process.env.PORT || 7000,
+        cacheMaxAge: 1
+    }); // cacheMaxAge: 10000
+} catch (error) {
+    console.error(error);
+}
