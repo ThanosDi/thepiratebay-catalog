80d1cf7664382f87a906064f127396cdc8ac7e04
ThanosDi <tdiamantidis@vivantehealth.com>
2020-12-30 12:10:20 +0200

    Change cache time

--- src/server.js
+++ src/server.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 const { serveHTTP } = require('stremio-addon-sdk');
-=======
-const {serveHTTP} = require('stremio-addon-sdk');
->>>>>>> ac92c627f6471b730a382c899a2a1fbd25e73421
 const addonInterface = require('./addon');
 
 require('events').EventEmitter.defaultMaxListeners = 50;
 const ONE_DAY = 86400;
 
 serveHTTP(addonInterface, {
-<<<<<<< HEAD
     port: process.env.PORT || 7000,
     cacheMaxAge: 1
 }); // cacheMaxAge: 10000
-=======
-	port: process.env.PORT || 7000,
-	cacheMaxAge: 10000
-}); // cacheMaxAge: 10000
->>>>>>> ac92c627f6471b730a382c899a2a1fbd25e73421
