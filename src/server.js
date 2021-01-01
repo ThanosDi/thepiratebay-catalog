1de8cb8cf0fa6157537a751f081a379f06d245ef
ThanosDi <tdiamantidis@vivantehealth.com>
2021-01-01 14:30:37 +0200

    add logs

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const ONE_DAY = 86400;
 try {
 	serveHTTP(addonInterface, {
 		port: process.env.PORT || 7000,
-		cacheMaxAge: 3600
+		cacheMaxAge: 10
 	}); // cacheMaxAge: 10000
 } catch (error) {
 	console.error(error);
