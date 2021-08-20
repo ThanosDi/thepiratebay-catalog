2ce97bee246ac77d5f3fb9a417653e878ba29816
PowerPC <thanosdi@live.com>
2021-08-20 08:23:33 +0300

    Update manifest version

--- src/manifest.js
+++ src/manifest.js
@@ -1,6 +1,6 @@
 module.exports = {
 	id: 'org.stremio.thepiratebay-catalog',
-	version: '1.1.0',
+	version: '1.1.1',
 	name: 'ThePirateBay Catalog',
 	description: 'Addon providing a catalog and search from The Pirate Bay.',
 	isFree: true,
